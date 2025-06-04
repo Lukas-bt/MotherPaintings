c207c51699cdd18a60133a4f61d8b3adac4093eb
Shift <atsukarisme@gmail.com>
2025-06-04 21:53:06 +0200

    (INIT) - Initialisation du projet

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+export default function navbar(){
+
+    return (
+        <nav>
+          <ul className="navbar">
+          <li>
+            <a>
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a>
+              Collections
+            </a>
+          </li>
+          </ul>
+        </nav>
+      );
+
+
+
+}

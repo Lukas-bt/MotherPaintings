42fc7de439b57b83fcd594a91d2670e56e1168a2
Shift <atsukarisme@gmail.com>
2025-06-08 19:42:15 +0200

    (EDIT) - Clear du background des img, responsive

--- src/components/PaintingCard.jsx
+++ src/components/PaintingCard.jsx
@@ -1,15 +1,15 @@
 export default function PaintingCard({ infos }) {
 
     return (
-        <div className="border-2 rounded p-5 border-black">
-            <div className="w-full h-64 overflow-hidden rounded-2xl">
+        <div className="p-7">
+            <div className="w-full h-64 overflow-hidden mt-auto mb-auto">
                 <img
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-contain"
                     src={"./images/" + infos.img}
                     alt={infos.name}
                 />
             </div>
-            <h1 className="text-3xl text-center mt-5">
+            <h1 className="text-2xl text-center mt-3">
                 {infos.name}
             </h1>
             <h2 className="text-center text-gray-700 italic">
